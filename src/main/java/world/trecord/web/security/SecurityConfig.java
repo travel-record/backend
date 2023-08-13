ff2f82b23fb01d810c6f6b86d546df34e02b423d
dragonappear <dragonappear1@gmail.com>
2023-08-13 17:25:11 +0900

    Modify: cors config

--- src/main/java/world/trecord/web/security/SecurityConfig.java
+++ src/main/java/world/trecord/web/security/SecurityConfig.java
@@ -60,10 +60,9 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOriginPatterns(List.of("*"));
+        configuration.setAllowedOriginPatterns(List.of("http://localhost:3000", "https://www.trecord.com"));
         configuration.setAllowedMethods(List.of("HEAD", "POST", "GET", "DELETE", "PATCH", "PUT", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
-        //configuration.setAllowedOriginPatterns(List.of("http://localhost:3000", "https://www.trecord.com"));
 //        configuration.setAllowCredentials(true);
 //        configuration.setMaxAge(Duration.ofHours(1));
 
