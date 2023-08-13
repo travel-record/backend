563da0fba7888d7f70d8ba7d15146316f2f2a161
dragonappear <dragonappear1@gmail.com>
2023-08-13 15:12:42 +0900

    Add: apply cors in request options

--- src/main/java/world/trecord/web/security/JwtAuthFilter.java
+++ src/main/java/world/trecord/web/security/JwtAuthFilter.java
@@ -32,12 +32,12 @@ public class JwtAuthFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
-        // CORS preflight 요청 처리
         if (request.getMethod().equalsIgnoreCase("OPTIONS")) {
             filterChain.doFilter(request, response);
+            response.addHeader("Access-Control-Allow-Origin", "*");
             return;
         }
-        
+
         String path = request.getRequestURI();
 
         try {
