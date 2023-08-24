fd4bc8b4cd6071e6c15b606abbb4916b1636f8c4
dragonappear <dragonappear1@gmail.com>
2023-08-24 09:10:59 +0900

    Modify: change validator exception
    - before: custom exception
    - after: bind exception
    - cause: impl for field error

--- src/main/java/world/trecord/web/controller/feed/FeedValidator.java
+++ src/main/java/world/trecord/web/controller/feed/FeedValidator.java
@@ -1,32 +1,35 @@
 package world.trecord.web.controller.feed;
 
 import org.springframework.stereotype.Component;
-import world.trecord.web.exception.CustomException;
+import org.springframework.validation.BindException;
+import org.springframework.validation.FieldError;
 import world.trecord.web.service.feed.request.FeedCreateRequest;
 import world.trecord.web.service.feed.request.FeedUpdateRequest;
-
-import static world.trecord.web.exception.CustomExceptionError.INVALID_ARGUMENT;
 
 @Component
 public class FeedValidator {
 
-    public void verify(FeedCreateRequest request) {
+    public void verify(FeedCreateRequest request) throws BindException {
         if (request.getStartAt() == null || request.getEndAt() == null) {
             return;
         }
 
         if (request.getStartAt().isAfter(request.getEndAt())) {
-            throw new CustomException(INVALID_ARGUMENT);
+            BindException bindException = new BindException(request, "feedCreateRequest");
+            bindException.addError(new FieldError("feedCreateRequest", "startAt", "Start time should be before end time."));
+            throw bindException;
         }
     }
 
-    public void verify(FeedUpdateRequest request) {
+    public void verify(FeedUpdateRequest request) throws BindException {
         if (request.getStartAt() == null || request.getEndAt() == null) {
             return;
         }
 
         if (request.getStartAt().isAfter(request.getEndAt())) {
-            throw new CustomException(INVALID_ARGUMENT);
+            BindException bindException = new BindException(request, "feedUpdateRequest");
+            bindException.addError(new FieldError("feedUpdateRequest", "startAt", "Start time should be before end time."));
+            throw bindException;
         }
     }
 }
--- src/test/java/world/trecord/web/controller/feed/FeedValidatorTest.java
+++ src/test/java/world/trecord/web/controller/feed/FeedValidatorTest.java
@@ -3,7 +3,7 @@ package world.trecord.web.controller.feed;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import world.trecord.web.exception.CustomException;
+import org.springframework.validation.BindException;
 import world.trecord.web.service.feed.request.FeedCreateRequest;
 import world.trecord.web.service.feed.request.FeedUpdateRequest;
 
@@ -15,32 +15,31 @@ class FeedValidatorTest {
     @DisplayName("피드 생성 요청할 때 여행 시작 시간이 여행 종료 시간보다 뒤에 있으면 예외가 발생한다")
     void validateFeedCreateRequestTest() throws Exception {
         //given
+        FeedValidator feedValidator = new FeedValidator();
+
         FeedCreateRequest request = FeedCreateRequest.builder()
                 .startAt(LocalDateTime.of(2022, 10, 31, 0, 0))
                 .endAt(LocalDateTime.of(2022, 10, 1, 0, 0))
                 .build();
 
-        FeedValidator feedValidator = new FeedValidator();
-
         //when //then
         Assertions.assertThatThrownBy(() -> feedValidator.verify(request))
-                .isInstanceOf(CustomException.class);
+                .isInstanceOf(BindException.class);
     }
 
     @Test
     @DisplayName("피드 업데이트 요청할 때 여행 시작 시간이 여행 종료 시간보다 뒤에 있으면 예외가 발생한다")
     void validateFeedUpdateRequestTest() throws Exception {
         //given
+        FeedValidator feedValidator = new FeedValidator();
+
         FeedUpdateRequest request = FeedUpdateRequest.builder()
                 .startAt(LocalDateTime.of(2022, 10, 31, 0, 0))
                 .endAt(LocalDateTime.of(2022, 10, 1, 0, 0))
                 .build();
 
-        FeedValidator feedValidator = new FeedValidator();
-
         //when //then
         Assertions.assertThatThrownBy(() -> feedValidator.verify(request))
-                .isInstanceOf(CustomException.class);
+                .isInstanceOf(BindException.class);
     }
-
 }
