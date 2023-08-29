5a7081588f6e25ea84c3524142f12593e851e45f
dragonappear <dragonappear1@gmail.com>
2023-08-29 20:28:53 +0900

    Modify: google feign client err decoder

--- src/main/java/world/trecord/web/client/feign/decoder/GoogleFeignErrorDecoder.java
+++ src/main/java/world/trecord/web/client/feign/decoder/GoogleFeignErrorDecoder.java
@@ -3,28 +3,43 @@ package world.trecord.web.client.feign.decoder;
 import feign.Response;
 import feign.Util;
 import feign.codec.ErrorDecoder;
-import org.springframework.http.HttpStatus;
 import world.trecord.web.exception.CustomException;
 
-import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Set;
 
+import static org.springframework.http.HttpStatus.*;
 import static world.trecord.web.exception.CustomExceptionError.INVALID_GOOGLE_AUTHORIZATION_CODE;
 
 public class GoogleFeignErrorDecoder implements ErrorDecoder {
 
+    private static final Set<Integer> GOOGLE_AUTHORIZATION_ERROR_STATUS = Set.of(
+            BAD_REQUEST.value(),
+            UNAUTHORIZED.value(),
+            FORBIDDEN.value()
+    );
+
     @Override
     public Exception decode(String methodKey, Response response) {
-        if (response.status() == HttpStatus.BAD_REQUEST.value()) {
+        if (isGoogleAuthorizationError(response.status())) {
             throw new CustomException(INVALID_GOOGLE_AUTHORIZATION_CODE);
         }
-
         throw new IllegalStateException(getErrorContent(response));
     }
 
+    private boolean isGoogleAuthorizationError(int status) {
+        return GOOGLE_AUTHORIZATION_ERROR_STATUS.contains(status);
+    }
+
     private String getErrorContent(Response response) {
+        if (response.body() == null) {
+            return "Response error body is null";
+        }
+
         try {
-            return Util.toString(response.body().asReader());
-        } catch (IOException e) {
+            byte[] bodyData = Util.toByteArray(response.body().asInputStream());
+            return new String(bodyData, StandardCharsets.UTF_8);
+        } catch (Exception e) {
             return "Failed to decode error content";
         }
     }
