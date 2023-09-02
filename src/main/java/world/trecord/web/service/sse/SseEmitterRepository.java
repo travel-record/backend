1e8878985a5d1c4941b9b7f3bb93676ef92175d3
dragonappear <dragonappear1@gmail.com>
2023-09-03 07:10:51 +0900

    Feat: apply concurrency control in sse emitter

--- src/main/java/world/trecord/web/service/sse/SseEmitterRepository.java
+++ src/main/java/world/trecord/web/service/sse/SseEmitterRepository.java
@@ -5,9 +5,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
 
 // TODO redis 사용
 @Slf4j
@@ -15,7 +15,7 @@ import java.util.Optional;
 @Repository
 public class SseEmitterRepository {
 
-    private Map<String, SseEmitter> emitterMap = new HashMap<>();
+    private Map<String, SseEmitter> emitterMap = new ConcurrentHashMap<>();
 
     public SseEmitter save(Long userId, SseEmitter emitter) {
         final String key = getKey(userId);
--- src/main/java/world/trecord/web/service/sse/SseEmitterService.java
+++ src/main/java/world/trecord/web/service/sse/SseEmitterService.java
@@ -13,24 +13,26 @@ import world.trecord.web.service.notification.NotificationService;
 
 import java.io.IOException;
 import java.util.Objects;
+import java.util.concurrent.atomic.AtomicInteger;
 
+import static world.trecord.web.exception.CustomExceptionError.MAX_CONNECTIONS_EXCEEDED_ERROR;
 import static world.trecord.web.exception.CustomExceptionError.NOTIFICATION_CONNECT_ERROR;
 
+// TODO 비동기 처리
 @Slf4j
 @RequiredArgsConstructor
 @Component
 public class SseEmitterService {
 
     public final static String EVENT_NAME = "notification";
+    private static final int MAX_CONNECTIONS = 1000;
+    private AtomicInteger currentConnections = new AtomicInteger(0);
 
     private final SseEmitterRepository sseEmitterRepository;
     private final NotificationService notificationService;
 
-    // TODO 비동기 처리
-
     @Transactional
     public void send(Long userToId, Long userFromId, NotificationType type, NotificationArgs args) {
-
         if (Objects.equals(userToId, userFromId)) {
             return;
         }
@@ -53,33 +55,61 @@ public class SseEmitterService {
     }
 
     public SseEmitter connect(Long userId, SseEmitter emitter) {
-        sseEmitterRepository.save(userId, emitter);
+        if (currentConnections.get() >= MAX_CONNECTIONS) {
+            throw new CustomException(MAX_CONNECTIONS_EXCEEDED_ERROR);
+        }
 
+        try {
+            incrementConnection();
+            establishConnection(userId, emitter);
+        } catch (Exception e) {
+            decrementConnection();
+            log.error("Error establishing SSE connection for user {}: {}", userId, e.getMessage());
+            throw new CustomException(NOTIFICATION_CONNECT_ERROR);
+        }
+
+        return emitter;
+    }
+
+    private void establishConnection(Long userId, SseEmitter emitter) throws IOException {
+        sseEmitterRepository.save(userId, emitter);
         emitter.onCompletion(() -> {
             log.info("SSE connection for user {} has been completed.", userId);
             sseEmitterRepository.delete(userId);
+            decrementConnection();
         });
 
         emitter.onTimeout(() -> {
             log.warn("SSE connection for user {} has timed out.", userId);
             sseEmitterRepository.delete(userId);
+            decrementConnection();
         });
 
+        emitter.onError((throwable) -> {
+            log.error("Error with SSE connection for user {}: {}", userId, throwable.getMessage());
+            sseEmitterRepository.delete(userId);
+            decrementConnection();
+        });
 
-        try {
-            log.info("Attempting to send connection completion event for user {}.", userId);
-            String eventID = String.valueOf(System.currentTimeMillis());
-            emitter.send(SseEmitter.event()
-                    .id(eventID)
-                    .name(EVENT_NAME)
-                    .data("Connection completed"));
-            log.info("Connection completion event for user {} has been sent successfully.", userId);
-        } catch (IOException exception) {
-            log.error("Error sending connection completion event for user {}: {}", userId, exception.getMessage());
-            throw new CustomException(NOTIFICATION_CONNECT_ERROR);
-        }
+        sendConnectionCompletionEvent(userId, emitter);
+    }
 
-        return emitter;
+    private void sendConnectionCompletionEvent(Long userId, SseEmitter emitter) throws IOException {
+        log.info("Attempting to send connection completion event for user {}.", userId);
+        String eventID = String.valueOf(System.currentTimeMillis());
+        emitter.send(SseEmitter.event()
+                .id(eventID)
+                .name(EVENT_NAME)
+                .data("Connection completed"));
+        log.info("Connection completion event for user {} has been sent successfully.", userId);
+    }
+
+    private void incrementConnection() {
+        currentConnections.incrementAndGet();
+    }
+
+    private void decrementConnection() {
+        currentConnections.decrementAndGet();
     }
 
     private SseNotificationEvent buildNotificationEvent(NotificationEntity notificationEntity) {
