48f466e65b3da822ac71b2ec1d4bd2e7c4359038
dragonappear <dragonappear1@gmail.com>
2023-09-04 11:31:44 +0900

    Modify: change thread pool size

--- src/main/java/world/trecord/config/AsyncConfig.java
+++ src/main/java/world/trecord/config/AsyncConfig.java
@@ -4,9 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@EnableTransactionManagement
 @EnableAsync
 @Configuration
 public class AsyncConfig {
@@ -14,8 +12,8 @@ public class AsyncConfig {
     @Bean(name = "sseTaskExecutor", destroyMethod = "shutdown")
     public ThreadPoolTaskExecutor messagingTaskExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(200);
-        executor.setMaxPoolSize(200);
+        executor.setCorePoolSize(10);
+        executor.setMaxPoolSize(50);
         return executor;
     }
 }
