7b4ce4d47d4bd8d6cd071dca7b87e8a709d65d36
dragonappear <dragonappear1@gmail.com>
2023-09-08 12:51:37 +0900

    Modify: change field name
    - isEditable -> isUpdatable

--- src/main/java/world/trecord/service/comment/response/CommentResponse.java
+++ src/main/java/world/trecord/service/comment/response/CommentResponse.java
@@ -18,7 +18,7 @@ public class CommentResponse {
     private Long parentId;
     private Long commentId;
     private String content;
-    private boolean isEditable;
+    private boolean isUpdatable;
 
     @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm")
     private LocalDateTime createdDateTime;
@@ -29,7 +29,7 @@ public class CommentResponse {
         this.parentId = commentEntity.getParentCommentEntity() != null ? commentEntity.getParentCommentEntity().getId() : null;
         this.commentId = commentEntity.getId();
         this.content = commentEntity.getContent();
-        this.isEditable = commentEntity.isCommenter(viewerId);
+        this.isUpdatable = commentEntity.isCommenter(viewerId);
         this.createdDateTime = commentEntity.getCreatedDateTime();
     }
 }
