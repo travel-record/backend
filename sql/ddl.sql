c9864292da949d52f514a9323eb3d0151eee78bd
dragonappear <dragonappear1@gmail.com>
2023-09-10 12:02:06 +0900

    Modify: ddl

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -58,8 +58,8 @@ create table notification
 
 create table record
 (
-    id_record          int auto_increment comment '기록 PK'
-        primary key,
+    id_record          int auto_increment comment '기록 PK' primary key,
+    id_author           int           not null comment '작성자 FK',
     content            longtext      not null comment '내용',
     date               datetime      not null comment '날짜',
     feeling            varchar(255)  not null comment '기분',
@@ -74,9 +74,8 @@ create table record
     created_date_time  datetime      null comment '기록 생성 시간',
     modified_date_time datetime      null comment '기록 수정 시간',
     deleted_date_time  datetime      null comment '기록 삭제 시간',
-    constraint fk_record_feed
-        foreign key (id_feed) references feed (id_feed)
-            on update cascade on delete cascade
+    constraint fk_record_feed foreign key (id_feed) references feed (id_feed) on update cascade on delete cascade,
+    constraint fk_record_users foreign key (id_author) references users (id_users) on update cascade on delete cascade
 )
     comment '기록';
 
@@ -91,15 +90,9 @@ create table comment
     created_date_time  datetime     null comment '댓글 생성 시간',
     modified_date_time datetime     null comment '댓글 수정 시간',
     deleted_date_time  datetime     null comment '댓글 삭제 시간',
-    constraint fk_comment_comment
-        foreign key (id_parent) references comment (id_comment)
-            on delete cascade,
-    constraint fk_comment_record
-        foreign key (id_record) references record (id_record)
-            on delete cascade,
-    constraint fk_comment_user
-        foreign key (id_users) references users (id_users)
-            on delete cascade
+    constraint fk_comment_comment foreign key (id_parent) references comment (id_comment) on delete cascade,
+    constraint fk_comment_record foreign key (id_record) references record (id_record) on delete cascade,
+    constraint fk_comment_user foreign key (id_users) references users (id_users) on delete cascade
 )
     comment '댓글';
 
@@ -144,3 +137,31 @@ create table user_record_like
 create index idx_user_id
     on user_record_like (id_users)
     comment '유저 PK 인덱스';
+
+create table feed_contributor
+(
+    created_date_time  datetime         comment '컨트리뷰터 생성 시간'          null,
+    deleted_date_time  datetime         comment '컨트리뷰터 삭제 시간'         null,
+    id_contributor     int auto_increment comment '컨트리뷰터 PK'
+        primary key,
+    id_feed            int           comment '피드 FK'            null,
+    id_users           int            comment '사용자 FK'           null,
+    modified_date_time datetime          comment '컨트리뷰터 수정 시간'       null,
+    permission         longtext collate utf8mb4_bin comment '컨트리뷰터 권한' null check (json_valid(`permission`)),
+    constraint fk_contributor_feed foreign key (id_feed) references feed (id_feed),
+    constraint fk_contributor_users foreign key (id_users) references users (id_users)
+) comment '컨트리뷰터' ;
+
+create table invitation
+(
+    created_date_time  datetime(6)        comment '초대 생성 시간'                         null,
+    deleted_date_time  datetime(6)         comment '초대 삭제 시간'                        null,
+    id_feed            int                comment '피드 FK'                      null,
+    id_invitation      int auto_increment comment '초대 PK' primary key,
+    id_users_to        int                   comment '사용자 FK'                   null,
+    modified_date_time datetime(6)           comment '초대 수정 시간'                      null,
+    status             varchar(20)    comment '초대 상태'  null,
+    constraint idx_invitation_users_feed unique (id_users_to, id_feed),
+    constraint fk_invitation_feed foreign key (id_feed) references feed (id_feed),
+    constraint fk_invitation_users foreign key (id_users_to) references users (id_users)
+) comment '피드 초대';
